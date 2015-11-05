fda469696217e358276e772440fe3183f4eb1f04
mr.mikky <hunglm_b01501@Fpt.aptech.ac.vn>
2015-11-05 09:47:19 +0700

    add billItem

---
+++ app/Payment/BillItem.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Payment;
+
+
+class BillItem implements \App\Contracts\Paying\BillItem
+{
+
+    /**
+     * @return string
+     */
+    public function name()
+    {
+        // TODO: Implement name() method.
+    }
+
+    /**
+     * @return int
+     */
+    public function quantity()
+    {
+        // TODO: Implement quantity() method.
+    }
+
+    /**
+     * @return float
+     */
+    public function price()
+    {
+        // TODO: Implement price() method.
+    }
+
+    /**
+     * @return float
+     */
+    public function totalAmount()
+    {
+        // TODO: Implement totalAmount() method.
+    }
+}
