72c76df208996cbc9cf7ec8e2c91d5f4a6b75f18
cuuthegioi <ngangonvadenho@gmail.com>
2015-11-05 11:52:06 +0700

    migration table Bill, BillItem, BillHistory

---
+++ database/migrations/2015_11_05_042956_create_bill_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBillTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('totalAmount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('bills');
+    }
+}
---
+++ database/migrations/2015_11_05_043816_create_bill_item_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBillItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bill_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('quantity');
+            $table->float('price');
+            $table->float('totalAmount');
+            $table->foreign('billId')->references('id')->on('bills');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('bill_items');
+    }
+}
