43e482f790829ab9a9c336c38e02435dd5ec65bc
mr.mikky <hunglm_b01501@Fpt.aptech.ac.vn>
2015-11-06 16:03:50 +0700

    delete Route billinsert

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,22 +18,3 @@ Route::get('/sku/{id}', function (\App\Shopping\SKURepository $repository, $id) 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/billinsert', function (\App\Payment\BillRepository $repository) {
-
-    $bill = new \App\Payment\Bill();
-    $sku = new \App\Shopping\SKU();
-    $member =  new \App\Member\Member();
-    $sku->setId(22);
-    $sku->setName("hung");
-    $sku->setOriginPrice(199);
-    $quantity = 5;
-    $billItem1 = new \App\Payment\BillItem($sku, $quantity);
-    $member->setId(12);
-    $bill->setBillItem($billItem1);
-    $bill->price();
-    $bill->setMember($member);
-    $repository->save($bill);
-
-    return view('welcome');
-});
